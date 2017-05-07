17736481f5c9a7502da341d5ce6c87258db49a83
John Mortlock <john.mortlock@gmail.com>
2017-05-07 15:15:44 +0930

    update test

--- test/trees_test.exs
+++ test/trees_test.exs
@@ -2,11 +2,17 @@ defmodule TreesTest do
   use ExUnit.Case
 
   setup do
-    [list: [
-      %{:id => 0, :name => "parent", :parent_id => nil},
-      %{:id => 1, :name => "child", :parent_id => 0},
-      %{:id => 2, :name => "second child", :parent_id => 1},
-    ]]
+    [
+      list_a: [
+        %{id: 0, parent_id: nil},
+        %{id: 1, parent_id: 0},
+        %{id: 2, parent_id: 1},
+      ],
+      list_b: [
+        %{id: 3, parent_id: nil},
+        %{id: 4, parent_id: 3 }
+      ]
+    ]
   end
 
   setup _context do
@@ -14,16 +20,24 @@ defmodule TreesTest do
   end
 
   test "can traverse children", context do
-    list = context[:list]
+    list = context[:list_a]
     root_node = List.first(list)
     children = Trees.List.children_of_node(root_node, list)
     assert children == [Enum.at(list, 1)] ++ [Enum.at(list, 2)]
   end
 
   test "can traverse parents", context do
-    list = context[:list]
+    list = context[:list_a]
     child_node = List.last(list)
     parents = Trees.List.parents_of_node(child_node, list)
     assert parents == [Enum.at(list, 1)] ++ [Enum.at(list, 0)]
+  end
+
+  test "can_handle multiple trees", context do
+    list = context[:list_a] ++ context[:list_b]
+    list_b = context[:list_b]
+    root_node = List.first(context[:list_b])
+    children = Trees.List.children_of_node(root_node, list)
+    assert children == [Enum.at(list_b, 1)]
   end
 end
